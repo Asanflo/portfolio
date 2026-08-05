226b0b9bcd441725a6647b7b83f55cafba7b5b32
Agassem Sanda Florentin <agassemflorentin@gmail.com>
2026-08-05 11:56:22 +0100

    fix: Chatbot agent

--- api/chat.js
+++ api/chat.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   if (!message) return res.status(400).json({ error: 'message requis' });
 
   try {
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
+    const url =`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
 
     const response = await fetch(url, {
       method: 'POST',
